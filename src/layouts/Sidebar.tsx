fc3fb5891684186f7a51385cb064d43b5a6a9a15
HelloHailie <alsltjsals@gmail.com>
2025-02-18 22:38:21 +0900

    feat: 상품 등록 페이지에서 다른 메뉴로 이동할 때 "저장하지 않고 페이지를 이동하겠습니까?"라는 경고 메시지가 표시

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -12,23 +12,31 @@ export default function Sidebar() {
     return location.pathname === path;
   };
 
+  const handleNavigation = (path: string) => {
+    if (location.pathname === PAGE_ROUTE.PRODUCT_REGISTER && path !== PAGE_ROUTE.PRODUCT) {
+      const confirmed = window.confirm('저장하지 않고 페이지를 이동하겠습니까?');
+      if (!confirmed) return;
+    }
+    nav(path);
+  };
+
   return (
     <ul className="p-5 flex flex-col gap-3">
       <li
         className={`w-[200px] h-[50px] flex items-center justify-center cursor-pointer ${checkLocationName(PAGE_ROUTE.PRODUCT) && 'font-bold bg-slate-200'} hover:font-bold hover:bg-slate-200`}
-        onClick={() => nav(PAGE_ROUTE.PRODUCT)}
+        onClick={() => handleNavigation(PAGE_ROUTE.PRODUCT)}
       >
         상품 관리
       </li>
       <li
         className={`w-[200px] h-[50px] flex items-center justify-center cursor-pointer ${checkLocationName(PAGE_ROUTE.ORDER) && 'font-bold bg-slate-200'} hover:font-bold hover:bg-slate-200`}
-        onClick={() => nav(PAGE_ROUTE.ORDER)}
+        onClick={() => handleNavigation(PAGE_ROUTE.ORDER)}
       >
         주문 관리
       </li>
       <li
         className={`w-[200px] h-[50px] flex items-center justify-center cursor-pointer ${checkLocationName(PAGE_ROUTE.BANNER) && 'font-bold bg-slate-200'} hover:font-bold hover:bg-slate-200`}
-        onClick={() => nav(PAGE_ROUTE.BANNER)}
+        onClick={() => handleNavigation(PAGE_ROUTE.BANNER)}
       >
         배너 관리
       </li>
