0eac5c77415f7f2a0af40092af355fa93fadee7e
김지훈 <kigpand@gmail.com>
2025-03-01 22:40:14 +0900

    -f 배너 리스트 UI 추가(mocking 기반)

--- src/pages/banner/Banner.tsx
+++ src/pages/banner/Banner.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { PAGE_ROUTE } from '../../utils/route';
+import { dummy } from '../../services/mocking';
 
 export default function Banner() {
   const navigate = useNavigate();
@@ -9,13 +10,29 @@ export default function Banner() {
   };
 
   return (
-    <div className="p-8">
+    <div className="flex flex-col gap-5 p-8 w-full items-end">
       <button
         onClick={handleRegisterClick}
-        className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors"
+        className="w-[100px] mb-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors"
       >
         배너 등록
       </button>
+      <div className="w-full p-4 bg-white shadow-lg rounded-lg">
+        <h2 className="text-xl font-semibold mb-4">배너 리스트</h2>
+        <ul className="w-full divide-gray-300">
+          {dummy.map((banner) => (
+            <li
+              key={banner.id}
+              className="p-4 rounded-md flex justify-between items-center cursor-pointer hover:bg-slate-200 "
+            >
+              <span className="text-lg font-medium w-1/4">{banner.title}</span>
+              <span className="text-sm text-gray-600 w-1/4">{banner.bannerType}</span>
+              <span className="text-sm text-gray-500 w-1/4">{new Date(banner.startDate).toLocaleDateString()}</span>
+              <span className="text-sm text-gray-500 w-1/4">{new Date(banner.endDate).toLocaleDateString()}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
