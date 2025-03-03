0e678f6464772d88c3367cfd178784d76f11b541
김지훈 <kigpand@gmail.com>
2025-03-03 13:04:52 +0900

    -f 배너 UI 수정

--- src/pages/banner/Banner.tsx
+++ src/pages/banner/Banner.tsx
@@ -24,13 +24,19 @@ export default function Banner() {
       <div className="w-full p-4 bg-white shadow-lg rounded-lg">
         <h2 className="text-xl font-semibold mb-4">배너 리스트</h2>
         <ul className="w-full divide-gray-300">
+          <li className="p-4 rounded-md flex justify-between items-center">
+            <span className="text-xs font-semibold text-gray-600 w-1/4">타이틀</span>
+            <span className="text-xs font-semibold text-gray-600 w-1/4">타입</span>
+            <span className="text-xs font-semibold text-gray-600 w-1/4">시작일</span>
+            <span className="text-xs font-semibold text-gray-600 w-1/4">종료일</span>
+          </li>
           {dummy.map((banner) => (
             <li
               key={banner.id}
               onClick={() => handleListClick(banner.id)}
               className="p-4 rounded-md flex justify-between items-center cursor-pointer hover:bg-slate-200 "
             >
-              <span className="text-lg font-medium w-1/4">{banner.title}</span>
+              <span className="text-sm text-gray-600 font-medium w-1/4 truncate">{banner.title}</span>
               <span className="text-sm text-gray-600 w-1/4">{banner.bannerType}</span>
               <span className="text-sm text-gray-500 w-1/4">{new Date(banner.startDate).toLocaleDateString()}</span>
               <span className="text-sm text-gray-500 w-1/4">{new Date(banner.endDate).toLocaleDateString()}</span>
